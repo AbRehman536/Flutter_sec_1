af161854271531017ddd2275bc46d7fc575ff785
Abdullah <abdullahrehman1019@gmail.com>
2025-08-11 13:41:17 -0600

    single selection done

--- lib/login.dart
+++ lib/login.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
   @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController pwdController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellowAccent,
       appBar: AppBar(
         backgroundColor: Colors.red,
         leading: Icon(Icons.arrow_back_ios_new),
@@ -18,15 +26,16 @@ class LoginView extends StatelessWidget {
       ),
       body: Column(children: [
         SizedBox(height: 10,),
-        Text("LOGIN PAGE",style: TextStyle(fontWeight: FontWeight.w700, fontSize: 25),),
+        Text("WELCOME TO LOGIN PAGE",style: TextStyle(fontWeight: FontWeight.w700, fontSize: 25),),
         SizedBox(height: 10,),
         Padding(
           padding: const EdgeInsets.all(20.0),
           child: TextField(
+            controller: emailController,
             keyboardType: TextInputType.emailAddress,
             decoration: InputDecoration(
               label: Text("Email"),
-              hintText: 'test@gmail.com',
+                hintText: 'test@gmail.com',
               prefixIcon: Icon(Icons.email),
               suffix: Icon(Icons.person),
               border: OutlineInputBorder(
@@ -39,6 +48,7 @@ class LoginView extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.all(20.0),
           child: TextField(
+            controller: pwdController,
             keyboardType: TextInputType.visiblePassword,
             decoration: InputDecoration(
               label: Text("Password"),
@@ -53,7 +63,23 @@ class LoginView extends StatelessWidget {
           ),
         ),
         SizedBox(height: 10,),
-        ElevatedButton(onPressed: (){}, child: Text("Login")),
+        ElevatedButton(onPressed: (){
+          if(emailController.text.isEmpty){
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Email cannot be empty")));
+            return ;
+          }
+          if(pwdController.text.isEmpty){
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Password cannot be empty")));
+            return ;
+          }
+          if(pwdController.text.length < 8){
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Password cannot less than 8 digits ")));
+            return ;
+          }
+        }, child: Text("Login")),
       ],),
     );
   }
