90b42a48e791b99e0527e14140e2afe94ac30bea
Abdullah <abdullahrehman1019@gmail.com>
2025-10-08 13:52:16 -0600

    Services done

--- lib/services/tasks.dart
+++ lib/services/tasks.dart
@@ -32,7 +32,36 @@ class TaskServices{
         .update({'isCompleted': true});
   }
   //GetAllTask
+  Stream<List<TaskModel>> getAllTask(){
+    return FirebaseFirestore.instance
+        .collection("TaskCollection")
+        .snapshots()
+        .map((taskList)=>taskList.docs
+        .map((taskJson)=> TaskModel.fromJson(taskJson.data()))
+        .toList(),
+    );
+  }
   //GetInCompletedTask
+  Stream<List<TaskModel>> getInCompletedTask(){
+    return FirebaseFirestore.instance
+        .collection("TaskCollection")
+        .where('isCompleted' , isEqualTo: false)
+        .snapshots()
+        .map((taskList)=> taskList.docs
+        .map((taskJson)=> TaskModel.fromJson(taskJson.data()))
+        .toList(),
+    );
+  }
   //GetCompletedTask
+  Stream<List<TaskModel>> getCompletedTask(){
+    return FirebaseFirestore.instance
+        .collection("TaskCollection")
+        .where('isCompleted' , isEqualTo: true)
+        .snapshots()
+        .map((taskList)=> taskList.docs
+    .map((taskJson)=> TaskModel.fromJson(taskJson.data()))
+        .toList(),
+    );
+  }
 
 }
